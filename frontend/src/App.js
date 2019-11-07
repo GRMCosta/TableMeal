df252fa7155ad5c3e11d6987a0c0b686970334ea
GRMCosta <rafa_gian2011@hotmail.com>
2019-11-07 16:06:17 -0300

    Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="containeras">
+    <div className="container">
       {//<img src={logo} alt="TableMeal"/>
       }
-      
+      <div className="content">
+      <form >
+        <label htmlFor="nome">NOME *</label>
+        <input type="nome" id="nome" placeholder="Nome"/>
+
+        <label htmlFor="email">E-MAIL *</label>
+        <input type="email" id="email" placeholder="E-mail"/>
+
+        <label htmlFor="senha">SENHA *</label>
+        <input type="senha" id="senha" placeholder="Senha"/>
+
+        <label htmlFor="cnpj">CNPJ *</label>
+        <input type="cnpj" id="cnpj" placeholder="CNPJ"/>
+        <button className="btn" type="submit">Entrar</button>
+      </form>
+      </div>
     </div>
   );
 }

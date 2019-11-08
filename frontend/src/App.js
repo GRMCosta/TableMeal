72b8d088d85984ebd9d706807429dbd7a9c3726e
GRMCosta <rafa_gian2011@hotmail.com>
2019-11-08 17:56:18 -0300

    Cadastrar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,49 @@
-import React from 'react';
+import React, { useState} from 'react';
+import api from './services/api';
 import './App.css';
 
 //import logo from './assets/'
 
 function App() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setSenha] = useState('');
+  const [cnpj, setCnpj] = useState('');
+
+
+
+
+  async function handleSubmit(event){
+    event.preventDefault();
+    
+    const response = await api.post('/sessions', {
+      name,
+      email,
+      password,
+      cnpj
+    });
+    console.log(response);
+  }
+
   return (
     <div className="container">
       {//<img src={logo} alt="TableMeal"/>
       }
       <div className="content">
-      <form >
-        <label htmlFor="nome">NOME *</label>
-        <input type="nome" id="nome" placeholder="Nome"/>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">NOME *</label>
+          <input type="name" id="name" placeholder="Nome" value={name} onChange={event => setName(event.target.value)}/>
 
         <label htmlFor="email">E-MAIL *</label>
-        <input type="email" id="email" placeholder="E-mail"/>
+        <input type="email" id="email" placeholder="E-mail" value={email} onChange={event => setEmail(event.target.value)}/>
 
-        <label htmlFor="senha">SENHA *</label>
-        <input type="senha" id="senha" placeholder="Senha"/>
+        <label htmlFor="password">SENHA *</label>
+        <input type="password" id="password" placeholder="Senha" value={password} onChange={event => setSenha(event.target.value)}/>
 
         <label htmlFor="cnpj">CNPJ *</label>
-        <input type="cnpj" id="cnpj" placeholder="CNPJ"/>
-        <button className="btn" type="submit">Entrar</button>
+        <input type="cnpj" id="cnpj" placeholder="CNPJ" value={cnpj} onChange={event => setCnpj(event.target.value)}/>
+        
+        <button className="btn" type="submit">Cadastrar</button>
       </form>
       </div>
     </div>

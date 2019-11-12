499025f5f16949c482e03706a4d09b71236bf3d1
MathChincha <mathchincha58@gmail.com>
2019-11-12 20:31:38 -0300

    Update
    Finalização das telas de Login e SignUp,
    começo da utilização do backend

---
+++ mobile/lib/Models/user_model.dart
@@ -0,0 +1,26 @@
+import 'package:scoped_model/scoped_model.dart';
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class UserModel extends Model {
+  bool isLoading = false;
+  Map<String, dynamic> userData = Map();
+
+  void signUp(Map<String, dynamic> userData, String pass,
+      VoidCallback onSuccess, VoidCallback onFailed) {
+    isLoading = true;
+    notifyListeners();
+  }
+
+  Future signIn() async {
+    isLoading = true;
+    notifyListeners();
+
+    await Future.delayed(Duration(seconds: 3));
+
+    isLoading = false;
+    notifyListeners();
+  }
+
+  void recoverPass() {}
+}

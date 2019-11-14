5bdcf1254c9b64fa60e9e9e1db96b6fcb0238a63
GRMCosta <rafa_gian2011@hotmail.com>
2019-11-14 17:44:41 -0300

    Arrumei uns treco

--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -1,24 +1,26 @@
-import React, {useState, useMemo} from 'react';
+import React, { useState, useMemo } from 'react';
 import api from '../../services/api';
+import NavBar from '../Navbar'
+import logo from '../../assets/logo.png'
 
 
 import camera from '../../assets/photo-camera.svg';
 
 import './styles.css'
 
-export default function New({history}){
+export default function New({ history }) {
     const [name, setName] = useState('');
     const [price, setPrice] = useState('');
     const [description, setDescription] = useState('');
-    const [type, setType] = useState('');
-    const [thumbnail , setThumbnail] = useState('');
+    const [type, setType] = useState('Prato');
+    const [thumbnail, setThumbnail] = useState('');
 
     const preview = useMemo(() => {
         return thumbnail ? URL.createObjectURL(thumbnail) : null;
-    },[thumbnail]
+    }, [thumbnail]
     )
 
-    async function handleSubmit(event){
+    async function handleSubmit(event) {
         event.preventDefault();
 
         const data = new FormData();
@@ -29,56 +31,67 @@ export default function New({history}){
         data.append('price', price);
         data.append('description', description);
         data.append('type', type);
-
         await api.post('/food', data, {
-            headers: {user_id}
+            headers: { user_id }
         })
-
         history.push('/Food');
     }
+
+    function sair() {
+        history.push('/');
+    }
+
     return (
-        <form onSubmit={handleSubmit}>
-            <label id="thumbnail" style={{ backgroundImage: `url(${preview})`}}
-            className={thumbnail ? 'has-thumbnail' : ''}
-            >
-                <input type="file" 
-                onChange={event => setThumbnail(event.target.files[0])}
-                />
-                <img src={camera} alt="Select img"/>
-            </label>
+        <>
+            <NavBar sair={() => sair()} />
+            <div className="container">
+                <img src={logo} alt="TableMeal" />
 
-            <label htmlFor="name">NOME DO PRATO *</label>
-            <input 
-            id="name"
-            placeholder="Prato"
-            value={name}
-            onChange={event => setName(event.target.value)}
-            />
+                <div className="content">
+                    <form onSubmit={handleSubmit}>
+                        <label id="thumbnail" style={{ backgroundImage: `url(${preview})` }}
+                            className={thumbnail ? 'has-thumbnail' : ''}
+                        >
+                            <input type="file"
+                                onChange={event => setThumbnail(event.target.files[0])}
+                            />
+                            <img src={camera} alt="Select img" />
+                        </label>
 
-            <label htmlFor="price">PREÇO *</label>
-            <input 
-                id="price"
-                 placeholder="Preço"
-                value={price}
-                onChange={event => setPrice(event.target.value)}
-            />
+                        <label htmlFor="name">NOME DO PRATO *</label>
+                        <input
+                            id="name"
+                            placeholder="Prato"
+                            value={name}
+                            onChange={event => setName(event.target.value)}
+                        />
 
-            <label htmlFor="price">DESCRIÇÃO *</label>
-            <input 
-                id="description"
-                 placeholder="Descrição"
-                value={description}
-                onChange={event => setDescription(event.target.value)}
-            />
+                        <label htmlFor="price">PREÇO *</label>
+                        <input
+                            id="price"
+                            placeholder="Preço"
+                            value={price}
+                            onChange={event => setPrice(event.target.value)}
+                        />
 
-            <label htmlFor="type">TIPO *</label>
-            <input 
-                id="type"
-                 placeholder="Tipo"
-                value={type}
-                onChange={event => setType(event.target.value)}
-            />
-            <button type="submit" className="btn">Cadastrar Prato</button>
-        </form>
+                        <label htmlFor="price">DESCRIÇÃO *</label>
+                        <input
+                            id="description"
+                            placeholder="Descrição"
+                            value={description}
+                            onChange={event => setDescription(event.target.value)}
+                        />
+
+                        <label htmlFor="type">TIPO *</label>
+                        <select id="type" onChange={event => setType(event.target.value)} >
+                            <option value="Prato">Prato</option>
+                            <option value="Sobremesa">Sobremesa</option>
+                            <option value="Refrigerante">Refrigerante</option>
+                        </select>
+                        <button type="submit" className="btn">Cadastrar Prato</button>
+                    </form>
+                </div>
+            </div>
+        </>
     )
 }

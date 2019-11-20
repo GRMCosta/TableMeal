f6dd023f5524ba4af7afed9b735e89d24ebea7db
MathChincha <mathchincha58@gmail.com>
2019-11-20 16:42:09 -0300

    Update
    Modificando a tela de login, signup e o menu para receber os dados da api.
    Api ainda não testada

--- mobile/lib/Screens/signup_screen.dart
+++ mobile/lib/Screens/signup_screen.dart
@@ -1,90 +1,148 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/Models/user_model.dart';
+import 'package:scoped_model/scoped_model.dart';
 
-class SignUpScreen extends StatelessWidget {
+class SignUpScreen extends StatefulWidget {
+  @override
+  _SignUpScreenState createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  static final _nameController = TextEditingController();
+  static final _emailController = TextEditingController();
+  static final _passController = TextEditingController();
+  static final _cpfController = TextEditingController();
+
   static final _formKey = GlobalKey<FormState>();
+  static final _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Criar Conta", style: TextStyle(color: Colors.white),
+        key: _scaffoldKey,
+        appBar: AppBar(
+          title: Text(
+            "Criar Conta",
+            style: TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.red,
         ),
-        centerTitle: true,
-        backgroundColor: Colors.red,
-      ),
-      backgroundColor: Colors.white,
-      body: Form(
-        key: _formKey,
-        child: ListView(
-          padding: EdgeInsets.all(10.0),
-          children: <Widget>[
-            TextFormField(
-              decoration: InputDecoration(hintText: "Nome Completo"),
-              validator: (text) {
-                if (text.isEmpty) return "Nome Inválido";
-                return null;
-              },
-            ),
-            SizedBox(
-              height: 16.0,
-            ),
-            TextFormField(
-              decoration: InputDecoration(hintText: "CPF"),
-              keyboardType: TextInputType.number,
-              validator: (text) {
-                if (text.isEmpty || text.length < 11) return "CPF Inválido";
-                return null;
-              },
-            ),
-            SizedBox(
-              height: 16.0,
-            ),
-            TextFormField(
-              decoration: InputDecoration(hintText: "E-mail"),
-              keyboardType: TextInputType.emailAddress,
-              validator: (text) {
-                if (text.isEmpty || text.contains("@"))
-                  return "E-mail Inválido";
-                return null;
-              },
-            ),
-            SizedBox(
-              height: 16.0,
-            ),
-            TextFormField(
-              decoration: InputDecoration(hintText: "Senha"),
-              obscureText: true,
-              validator: (text) {
-                if (text.isEmpty || text.length < 6) return "Senha Inválida";
-                return null;
-              },
-            ),
-            SizedBox(
-              height: 16.0,
-            ),
-            SizedBox(
-              height: 44.0,
-              child: RaisedButton(
-                child: Text(
-                  "Criar Conta",
-                  style: TextStyle(
-                    fontSize: 18.0,
+        backgroundColor: Colors.white,
+        body:
+            ScopedModelDescendant<UserModel>(builder: (context, child, model) {
+          if (model.isLoading)
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+
+          return Form(
+            key: _formKey,
+            child: ListView(
+              padding: EdgeInsets.all(10.0),
+              children: <Widget>[
+                TextFormField(
+                  controller: _nameController,
+                  decoration: InputDecoration(hintText: "Nome Completo"),
+                  validator: (text) {
+                    if (text.isEmpty) return "Nome Inválido";
+                    return null;
+                  },
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                TextFormField(
+                  controller: _cpfController,
+                  decoration: InputDecoration(hintText: "CPF"),
+                  keyboardType: TextInputType.number,
+                  validator: (text) {
+                    if (text.isEmpty || text.length < 11) return "CPF Inválido";
+                    return null;
+                  },
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                TextFormField(
+                  controller: _emailController,
+                  decoration: InputDecoration(hintText: "E-mail"),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (text) {
+                    if (text.isEmpty || !text.contains("@"))
+                      return "E-mail Inválido";
+                    return null;
+                  },
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                TextFormField(
+                  controller: _passController,
+                  decoration: InputDecoration(hintText: "Senha"),
+                  obscureText: true,
+                  validator: (text) {
+                    if (text.isEmpty || text.length < 6)
+                      return "Senha Inválida";
+                    return null;
+                  },
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                SizedBox(
+                  height: 44.0,
+                  child: RaisedButton(
+                    child: Text(
+                      "Criar Conta",
+                      style: TextStyle(
+                        fontSize: 18.0,
+                      ),
+                    ),
+                    textColor: Colors.white,
+                    color: Colors.red,
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        Map<String, dynamic> userData = {
+                          "name": _nameController.text,
+                          "email": _emailController,
+                          "cpf": _cpfController,
+                        };
+
+                        model.signUp(
+                            userData: userData,
+                            pass: _passController.text,
+                            onSuccess: _onSuccess,
+                            onFail: _onFail);
+                      }
+                    },
                   ),
                 ),
-                textColor: Colors.white,
-                color: Colors.red,
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {
-
-                  }
-
-                },
-              ),
+              ],
             ),
-          ],
-        ),
-      ),
-    );
+          );
+        }));
+  }
+
+  void _onSuccess() {
+    _scaffoldKey.currentState.showSnackBar(SnackBar(
+      content: Text("Usuário criado com sucesso"),
+      backgroundColor: Theme.of(context).primaryColor,
+      duration: Duration(seconds: 2),
+    ));
+    Future.delayed(Duration(seconds: 2)).then((_) {
+      Navigator.of(context).pop();
+    });
+  }
+
+  void _onFail() {
+    _scaffoldKey.currentState.showSnackBar(SnackBar(
+      content: Text("Falha ao criar usuário"),
+      backgroundColor: Theme.of(context).primaryColor,
+      duration: Duration(seconds: 2),
+    ));
+    Future.delayed(Duration(seconds: 2)).then((_) {
+      Navigator.of(context).pop();
+    });
   }
 }
--- mobile/lib/widgets/custom_drawer.dart
+++ mobile/lib/widgets/custom_drawer.dart
@@ -1,35 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/Models/user_model.dart';
 import 'package:mobile/Screens/login_screen.dart';
 import 'package:mobile/tiles/drawer_tile.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class CustomDrawer extends StatelessWidget {
-
   final PageController pageController;
 
   CustomDrawer(this.pageController);
 
   @override
   Widget build(BuildContext context) {
-
     Widget _buildDrawerBack() => Container(
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-              colors: [
-                Colors.red,
-                Colors.pink,
-              ],
-              begin: Alignment.center,
-              end: Alignment.bottomCenter,
-          )
-      ),
-    );
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+            colors: [
+              Colors.red,
+              Colors.pink,
+            ],
+            begin: Alignment.center,
+            end: Alignment.bottomCenter,
+          )),
+        );
 
     return Drawer(
       child: Stack(
         children: <Widget>[
           _buildDrawerBack(),
           ListView(
-            padding: EdgeInsets.only( left: 32.0, top: 16.0),
+            padding: EdgeInsets.only(left: 32.0, top: 16.0),
             children: <Widget>[
               Container(
                 margin: EdgeInsets.only(bottom: 8.0),
@@ -40,41 +39,49 @@ class CustomDrawer extends StatelessWidget {
                     Positioned(
                       top: 8.0,
                       left: 0.0,
-                      child: Text("Flutter's \nTableMeal",
-                        style: TextStyle(fontSize: 34.0, fontWeight: FontWeight.bold),
+                      child: Text(
+                        "Flutter's \nTableMeal",
+                        style: TextStyle(
+                            fontSize: 34.0, fontWeight: FontWeight.bold),
                       ),
                     ),
                     Positioned(
-                      left: 0.0,
-                      bottom: 0.0,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text("Olá",
-                            style: TextStyle(
-                                fontSize: 20.0,
-                                fontWeight: FontWeight.bold
-                            ),
-                          ),
-                          GestureDetector(
-                            child: Text(
-                              "Entre ou cadastre-se",
-                              style: TextStyle(
-                                  color: Colors.indigo,
-                                  fontSize: 16.0,
-                                  fontWeight: FontWeight.bold
+                        left: 0.0,
+                        bottom: 0.0,
+                        child: ScopedModelDescendant<UserModel>(
+                            builder: (context, child, model) {
+                          return Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                "Olá, ${!model.isLoggedIn ? "" : model.userData["name"]}",
+                                style: TextStyle(
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.bold),
                               ),
-                            ),
-                            onTap: (){
-                              Navigator.of(context).push(
-                                MaterialPageRoute(builder: (context)=>LoginScreen())
-                              );
-                            },
-                          ),
-
-                        ],
-                      ),
-                    )
+                              GestureDetector(
+                                child: Text(
+                                  !model.isLoggedIn
+                                      ? "Entre ou cadastre-se"
+                                      : "Sair",
+                                  style: TextStyle(
+                                      color: Colors.indigo,
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                onTap: () {
+                                  if (model.isLoggedIn == false)
+                                    Navigator.of(context).push(
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                LoginScreen()));
+                                  else
+                                    model.signOut();
+                                },
+                              ),
+                            ],
+                          );
+                        }))
                   ],
                 ),
               ),
@@ -82,7 +89,8 @@ class CustomDrawer extends StatelessWidget {
               DrawerTile(Icons.home, "Início", pageController, 0),
               DrawerTile(Icons.list, "Cardápio", pageController, 1),
               DrawerTile(Icons.location_on, "Lojas", pageController, 2),
-              DrawerTile(Icons.playlist_add_check, "Meus Pedidos", pageController, 3),
+              DrawerTile(
+                  Icons.playlist_add_check, "Meus Pedidos", pageController, 3),
             ],
           )
         ],
@@ -90,5 +98,3 @@ class CustomDrawer extends StatelessWidget {
     );
   }
 }
-
-

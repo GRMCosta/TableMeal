d20cf337d438b82d786eba60dae2cf9cb460ee1b
GRMCosta <rafa_gian2011@hotmail.com>
2019-12-03 18:59:10 -0300

    Routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -23,6 +23,7 @@ routes.delete('/food', FoodController.delete);
 
 routes.get('/order', OrderController.index);
 routes.post('/order', OrderController.store);
+
 routes.put('/order', OrderController.update);
 
 
